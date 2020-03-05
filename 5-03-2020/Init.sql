499d4e749d62c4696b13044e240bb094c304784e
achu-venkat <ashwini.venkat@outlook.com>
2020-03-05 21:09:11 +1300

    updating bad code
    updating bad code

--- 5-03-2020/Init.sql
+++ 5-03-2020/Init.sql
@@ -1,8 +1,5 @@
-vSELECT c.CityID , c.CityName, COUNT(c2.CustomerID) 
+SELECT c.CityID , c.CityName, COUNT(c2.CustomerID) 
 FROM Application.Cities c
      JOIN Sales.Customers c2 ON c.CityID = c2.DeliveryCityID
 GROUP BY c.CityID, c.CityName
-HAVING COUNT(c2.CustomerID)  1
-
---655
---663
+HAVING COUNT(c2.CustomerID)  > 1
